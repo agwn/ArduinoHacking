25877c109829d592982cf4e83f929814e2a62f2d
Naim Busek <ndbusek@gmail.com>
2011-12-31 22:41:53 -0800

    first commit

---
+++ fucked/const_cpp_bug/const_cpp_bug.ino
@@ -0,0 +1,25 @@
+/*
+ Arduino preprocessor bug
+ Crashes the build, repeatedly.
+ 
+ This example code is in the public domain.
+ */
+
+typedef char fixed_t;
+
+#define TEST_DEFS 0
+
+#if TEST_DEFS
+static const char* test[1] = {
+  "test",
+};
+#endif
+
+void setup() {
+}
+
+void loop() {
+  delay(1000);              // wait for a second
+}
+
+
